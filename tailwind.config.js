48633daa36be40b0ae8c644a9295bede7abc693e
121038217 <125855991+121038217@users.noreply.github.com>
2024-03-11 03:34:11 -0600

    node

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./*.html'],
+  content: ['./*.html',
+  './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+  './vendor/laravel/jetstream/**/*.blade.php',
+  './storage/framework/views/*.php',
+  './resources/views/**/*.blade.php',
+],
   theme: {
     extend: {},
   },
